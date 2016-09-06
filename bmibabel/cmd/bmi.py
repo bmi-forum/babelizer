f0307ba861246507d8935b7913a9874fd454b0b4
mcflugen <mcflugen@gmail.com>
2016-09-06 17:31:23 -0600

    Added bmi sub-commands.

--- bmibabel/cmd/bmi.py
+++ bmibabel/cmd/bmi.py
@@ -2,8 +2,15 @@ from __future__ import print_function
 
 from .bmi_babelize import babelize
 from .bmi_find import find
+from .bmi_parameters import print_defaults
+from .bmi_stage import stage_model
+from .bmi_files import print_data_files
+
 from .bmi_babelize import add_arguments as babelize_add_arguments
 from .bmi_find import add_arguments as find_add_arguments
+from .bmi_parameters import add_arguments as parameters_add_arguments
+from .bmi_stage import add_arguments as stage_add_arguments
+from .bmi_files import add_arguments as files_add_arguments
 
 
 def main():
@@ -23,6 +30,21 @@ def main():
         'babelize', help='use babel to build language bindings')
     babelize_add_arguments(parser_babelize)
     parser_babelize.set_defaults(func=babelize)
+
+    parser_parameters = subparsers.add_parser(
+        'parameters', help='get model parameters')
+    parameters_add_arguments(parser_parameters)
+    parser_parameters.set_defaults(func=print_defaults)
+
+    parser_stage = subparsers.add_parser(
+        'stage', help='stage a model')
+    stage_add_arguments(parser_stage)
+    parser_stage.set_defaults(func=stage_model)
+
+    parser_files = subparsers.add_parser(
+        'files', help='BMI data files')
+    files_add_arguments(parser_files)
+    parser_files.set_defaults(func=print_data_files)
 
     args = parser.parse_args()
 
